cd2b76acf0ef09130b8368ecf1b0f9a3997d41d6
Paila veera venkata siva kumar <veeravenkatasivakumar.paila@gmail.com>
2026-01-21 20:14:36 +0530

    Update MovieController.java

--- TicketBooking1-main/TicketBooking/src/main/java/com/sat/tmf/tkt/tktbooking/controller/MovieController.java
+++ TicketBooking1-main/TicketBooking/src/main/java/com/sat/tmf/tkt/tktbooking/controller/MovieController.java
@@ -1,1 +1,28 @@
+package com.sat.tmf.tkt.tktbooking.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@Controller
+@RequestMapping("/movie")
+public class MovieController {
+	
+	@RequestMapping(value="/test", method = RequestMethod.GET)
+	public String testMethod(@RequestParam("mname") String movieName) {
+		System.out.println("This is a testMethod() @ MovieController");
+		System.out.println("Movie Name:"+movieName);
+		return null;
+	}
+	
+	@RequestMapping(value="/test", method = RequestMethod.POST)
+	public String testPostMethod(@RequestParam("mname") String movieName) {
+		System.out.println("This is a testMethod() @ MovieController");
+		System.out.println("Movie Name:"+movieName);
+		return null;
+	}
+}
+
+
 
